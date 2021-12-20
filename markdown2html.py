eeefd1b8632531d248e9dbe21560ff192b259182
faykris <faykris28@gmail.com>
2021-12-20 10:44:53 -0500

    [Update] markdown2html.py: fix missing markdown file message

--- markdown2html.py
+++ markdown2html.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
     if not os.path.exists(sys.argv[1]):
-        sys.stderr.write("Missing {}".format(sys.argv[1]))
+        sys.stderr.write("Missing {}\n".format(sys.argv[1]))
         exit(1)
 
     # Input file
